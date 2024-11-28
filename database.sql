f06d90ac51606c1a3bf9523a55e1a5b9974678cd
Erick Rivera <yomellamoerick.771@gmail.com>
2024-11-28 19:43:02 -0300

    modelo de datos

---
+++ database.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE imc_db;
+
+CREATE TABLE public.usuarios
+(
+    id serial NOT NULL,
+    nombre character varying(100),
+    clave character varying(100),
+    correo character varying(150),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.usuarios
+    OWNER to postgres;
+
+---------------
+
+CREATE TABLE public.resultado_imc
+(
+    id serial NOT NULL,
+    user_id integer NOT NULL,
+    peso double precision NOT NULL,
+    altura double precision NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.resultado_imc
+    OWNER to postgres;
+
+--------------------------
+-- Alteracion de unicidad para correo
+ALTER TABLE IF EXISTS public.usuarios
+    ADD CONSTRAINT correo_unique UNIQUE (correo)
+    INCLUDE (correo);
+
+----------------------------
+-- Alteracion para crear columnas de timestamp e imc
+-- Creacion de la fk para unir usuarios e imc
+
+ALTER TABLE IF EXISTS public.resultado_imc
+    ADD COLUMN creado_en timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE IF EXISTS public.resultado_imc
+    ADD COLUMN imc double precision NOT NULL;
+ALTER TABLE IF EXISTS public.resultado_imc
+    ADD CONSTRAINT fk_user_id_imc FOREIGN KEY (user_id)
+    REFERENCES public.usuarios (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
---
+++ src/main/java/modelo/DBConn.java
@@ -0,0 +1,47 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBConn {
+
+  private static Connection cnx = null;
+
+  private DBConn(){
+    try {
+      Class.forName("org.postgresql.Driver");
+
+      cnx = DriverManager.getConnection("jdbc:postgresql:imc_db?user=postgres&password=12345678");
+
+      System.out.println("Cargo el driver...");
+
+      if (cnx != null) {
+        System.out.println("Conexion establecida");
+      } else{
+        System.out.println("No se pudo conectar...");
+      }
+
+    } catch (SQLException e){
+      e.printStackTrace();
+    } catch (ClassNotFoundException e){
+      System.out.println(e.getMessage());
+    }
+  }
+
+  public static Connection getConnection(){
+
+    if (cnx == null) {
+      new DBConn();
+      System.out.println("CARGADO EL PRIMERO");
+    }
+
+    return cnx;
+  }
+
+  public static void main(String[] args) {
+    DBConn.getConnection();
+   }
+
+}
